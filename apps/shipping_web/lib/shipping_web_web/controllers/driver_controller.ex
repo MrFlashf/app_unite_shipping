f333460905f407a74fa3a11898d012353802c6da
Krzychu <krz.jozefowicz@gmail.com>
2018-02-19 14:09:16 +0100

    add send_load_request to DriverController

--- apps/shipping_web/lib/shipping_web_web/controllers/driver_controller.ex
+++ apps/shipping_web/lib/shipping_web_web/controllers/driver_controller.ex
@@ -1,7 +1,45 @@
 defmodule ShippingWebWeb.DriverController do
   use ShippingWebWeb, :controller
 
-  def index(conn, _params) do
-    render conn, "index.html"
+  alias Shipping.Shipper.Load
+  alias Shipping.Shipper
+  alias Shipping.Driver.Events.LoadRequestSent
+
+
+  @load %{
+    uuid: "uuid",
+    shipper_id: "shiper_id",
+    number_of_trips: "number_of_trips",
+    car_type: "car type",
+    start_date_millis: "start_date",
+    lat: "lat",
+    lng: "lng"
+  }
+
+  @load_request_sent %{
+    uuid: nil,
+    driver_id: nil,
+    load_id: nil,
+    timestamp: nil
+  }
+
+
+  def send_load_request(conn, params) do
+    load = struct(Load, @load)
+    load_request = struct(LoadRequestSent, %{uuid: params["uuid"], driver_id: params["driver_id"], load_id: params["load_id"], timestamp: params["timestamp"]})
+    {events, load} = Shipper.handle_load_request(load, load_request)
+    render conn, "load.json", load: load
+  end
+
+  def pick_up_load(conn, params) do
+
+  end
+
+  def change_vehicle_position(conn, params) do
+
+  end
+
+  def deliver_load(conn, params) do
+
   end
 end
